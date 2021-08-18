07f0848a734362dafb77a209063b38323256883a
Cristian Guaman <cristian.guaman@icloud.com>
2021-08-17 22:59:37 -0500

    Refactoring command

--- app/Console/Commands/questionsAndAnswersCommand.php
+++ app/Console/Commands/questionsAndAnswersCommand.php
@@ -6,38 +6,19 @@ use App\Models\Question;
 use App\Models\User;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Hash;
+use Symfony\Component\Console\Helper\Table;
+use Symfony\Component\Console\Helper\TableCell;
+use Symfony\Component\Console\Helper\TableSeparator;
 
 class questionsAndAnswersCommand extends Command
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
+
     protected $signature = 'qanda:interactive {user=guest@test.com}';
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
     protected $description = 'Execute Questions and Answers Program in an interactive mode';
 
-    /**
-     * Create a new command instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        parent::__construct();
-    }
+    protected $run = true;
 
-    /**
-     * Execute the console command.
-     *
-     * @return int
-     */
     public function handle()
     {
         $emailUser = $this->argument('user');
@@ -46,19 +27,18 @@ class questionsAndAnswersCommand extends Command
 
         $this->info("*** Welcome " . $user->name . " ***");
         $this->menu($user);
-
-        return 0;
     }
 
     protected function menu($user){
         $choice = $this->choice(
-            'Choose an option between [0-4], or write "exit" to stop this program',
+            'Choose an option between 0-5',
             [
                 'Create a question',
                 'List all questions',
                 'Practice',
                 'Stats',
                 'Reset',
+                'Exit',
             ],
             0
         );
@@ -73,16 +53,18 @@ class questionsAndAnswersCommand extends Command
             case "Practice":
                 $this->practice($user);
                 break;
-            case "exit":
-                $this->stop();
+            case "Exit":
+                $this->stop($user);
                 break;
+            default:
+                $this->menu($user);
         }
     }
 
     protected function createQuestion($user){
         $this->info('Creating a question...');
-        $question = $this->ask('Write a question')||"";
-        $answer = $this->ask('Write a answer to the previous question')||"";
+        $question = $this->ask('Write a question');
+        $answer = $this->ask('Write a answer to the previous question');
         Question::create([
             'description'=> $question,
             'answer' => $answer,
@@ -106,24 +88,45 @@ class questionsAndAnswersCommand extends Command
      */
     protected function practice($user){
         $this->info( "Practicing...");
-        $questions = Question::where('status','!=','NOT ANSWERED')->get();
-
-        $this->table(
-            ['Question', 'Answer', 'Status'],
-            Question::select('description', 'answer', 'status')->where('user_id', $user->id)->get()
-        );
-
-        $bar = $this->output->createProgressBar(count($questions));
-
+//        $questions = Question::where('user_id', $user->id)->get();
+        $allQuestions = Question::select('id','description', 'status')->where('user_id', $user->id)->get();
+        $answeredQuestions= Question::where('status','!=','NOT ANSWERED')->get();
+        $correctQuestions= Question::where('status','CORRECT')->get();
+        $separator = new TableSeparator();
+        /*$this->table(
+             ['Question', 'Answer', 'Status'],
+             Question::select('description', 'answer', 'status')->where('user_id', $user->id)->get(),
+         );*/
+//        dd($questions);
+//        $this->withProgressBar($allQuestions, function ($question) {
+////            $this->performTask($question);
+////            sleep(1);
+//        });
+//        dd($correctQuestions->toArray());
+        $bar = $this->output->createProgressBar($allQuestions->count());
         $bar->start();
-
-        foreach ($questions as $question) {
+        foreach ($correctQuestions->toArray() as $question) {
 //            $this->performTask($question);
-
+//            sleep(1);
             $bar->advance();
         }
-
         $bar->finish();
+
+        $this->newLine();
+        $table = new Table($this->output);
+        $table->setHeaders(['ID', 'Question', 'Status']);
+
+        $table->setRows(
+            $allQuestions->toArray()
+        );
+//        $table->setFooterTitle('Progress: '.count($correctQuestions)."/".count($allQuestions));
+        $table->setRow( count($allQuestions->toArray()), [$separator, $separator, $separator]);
+        $table->setRow( count($allQuestions->toArray())+1, [new TableCell( count($correctQuestions)."/".count($allQuestions), ['colspan'=>3])]);
+        $table->render();
+
+        $this->info("bye Practicing");
+
+
     }
 
     /**
@@ -145,7 +148,9 @@ class questionsAndAnswersCommand extends Command
         return User::where('email', $emailUser)->firstOrFail();
     }
 
-    protected function stop(){
-        $this->info("Exiting...");
+    protected function stop($user){
+        $this->info("Bye bye ".$user->name."...");
+        $this-> run = false;
+        exit();
     }
 }
